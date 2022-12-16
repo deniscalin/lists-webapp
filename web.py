8b174d143ea203ba2c6989af92ee5c6b3c730f2d
deniscalin <denis.calin@braze.com>
2022-12-16 13:58:56 -0800

    Add HTML support, some color and the page title

--- web.py
+++ web.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 todos = functions.get_todos()
 
+streamlit.set_page_config(page_title="Nulist Idea Mapping Service")
+
 
 def add_todo():
     todo = streamlit.session_state["new_todo"] + "\n"
@@ -12,7 +14,12 @@ def add_todo():
 
 
 streamlit.title("Nulist Idea Mapping Service")
-streamlit.subheader("Control your day! Add todo.")
+#streamlit.subheader("Control your day! Add todo.")
+streamlit.write("<h2> Control your day! Add a <FONT COLOR='0076BA'>t</FONT><FONT COLOR='970E53'>o</FONT><FONT COLOR='F27200'>d</FONT><FONT COLOR='004D80'>o</FONT><FONT COLOR='017100'>.</FONT></h2>",
+                unsafe_allow_html=True)
+
+streamlit.text_input(label="Enter your todo", placeholder="Add todo", on_change=add_todo,
+              key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = streamlit.checkbox(todo, key=todo)
@@ -21,11 +28,6 @@ for index, todo in enumerate(todos):
         functions.write(todos)
         del streamlit.session_state[todo]
         streamlit.experimental_rerun()
-
-
-streamlit.text_input(label="Enter your todo", placeholder="Add todo", on_change=add_todo,
-              key="new_todo")
-
 
 image = Image.open('nulist_logo_n.png')
 #streamlit.image(image, width=100)
