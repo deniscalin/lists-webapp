a6b2679d93b75feba83aa90cbe21a2386edfa694
deniscalin <denis.calin@braze.com>
2022-12-25 22:16:12 -0800

    Add minor code style fixes

--- web.py
+++ web.py
@@ -14,12 +14,13 @@ def add_todo():
 
 
 streamlit.title("Nulist Idea Mapping Service")
-#streamlit.subheader("Control your day! Add todo.")
-streamlit.write("<h2> Control your day! Add a <FONT COLOR='0076BA'>t</FONT><FONT COLOR='970E53'>o</FONT><FONT COLOR='F27200'>d</FONT><FONT COLOR='004D80'>o</FONT><FONT COLOR='017100'>.</FONT></h2>",
+# streamlit.subheader("Control your day! Add todo.")
+streamlit.write("<h2> Control your day! Add a <FONT COLOR='0076BA'>t</FONT><FONT COLOR='970E53'>o</FONT>"
+                "<FONT COLOR='F27200'>d</FONT><FONT COLOR='004D80'>o</FONT><FONT COLOR='017100'>.</FONT></h2>",
                 unsafe_allow_html=True)
 
 streamlit.text_input(label="Enter your todo", placeholder="Add todo", on_change=add_todo,
-              key="new_todo")
+                     key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = streamlit.checkbox(todo, key=todo)
@@ -30,7 +31,7 @@ for index, todo in enumerate(todos):
         streamlit.experimental_rerun()
 
 image = Image.open('nulist_logo_n.png')
-#streamlit.image(image, width=100)
+# streamlit.image(image, width=100)
 
 col1, col2, col3 = streamlit.columns([0.2, 5, 0.5])
 col3.image(image, use_column_width=True)
